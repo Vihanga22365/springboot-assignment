10590ce5bcb24c3886501109579fa1bbbb2e651a
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2023-06-09 12:42:20 +0530

    Create Excel Uploader and Downloader

---
+++ sheduler-service/src/main/java/com/virtusa/vihanga/shedulerservice/ShedulerServiceApplication.java
@@ -0,0 +1,15 @@
+package com.virtusa.vihanga.shedulerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class ShedulerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShedulerServiceApplication.class, args);
+	}
+
+}
---
+++ sheduler-service/src/test/java/com/virtusa/vihanga/shedulerservice/ShedulerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.vihanga.shedulerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShedulerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
