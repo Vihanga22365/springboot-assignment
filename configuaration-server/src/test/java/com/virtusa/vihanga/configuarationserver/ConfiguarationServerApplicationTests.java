3da058a408835469f11abc6ad51ffc112dddb92e
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2023-06-19 14:07:56 +0530

    Config Server Updates

---
+++ configuaration-server/src/test/java/com/virtusa/vihanga/configuarationserver/ConfiguarationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.vihanga.configuarationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguarationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
